7bd7229cde39e3eceb98c830393c7d94ff50e3cd
Mujahid <mujahidramdhani@gmail.com>
2025-02-27 19:11:04 +0700

    update porto

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -9,31 +9,43 @@ import furnicraft from "../../assets/furnicraft.png"
 import palmMapping from "../../assets/palmmapping.png"
 import programbantuan from "../../assets/programbantuan.png"
 import survey from "../../assets/survey.png"
+import erp from "../../assets/ERP.jpg"
 export function Project() {
   const data = [
     {
       id: 1,
-      image: palmMapping,
-      title: "SISTEM PEMETAAN “PALM MAPPING”",
-      points: ["Hyperledger Fabric", "Build Api", "expressJs", "reactJs", "mysql", "docker", "leaftletjs", "mapbox", "tailwind", "deployment google cloud"],
+      image: erp,
+      title: "ERP MODULE QUALITY CONTROL",
+      points: ["PHP", "AJAX JQUERY", "JAVASCRIT", "EASYUI", "MYSQL"],
+      github: "https://github.com/MujahidRamdhani",
     },
     {
       id: 2,
+      image: palmMapping,
+      title: "SISTEM PEMETAAN “PALM MAPPING”",
+      points: ["Hyperledger Fabric", "Build Api", "expressJs", "reactJs", "mysql", "docker", "leaftletjs", "mapbox", "tailwind", "deployment google cloud"],
+      github: "https://github.com/MujahidRamdhani/frontend-palmmapping/tree/main",
+    },
+    {
+      id: 3,
       image: programbantuan,
       title: "Program Kemensos bantuan masyarakat",
       points: ["build api", "express Js", "react Js", "mysql", "docker", "integration email notification breevo", "tailwind" , "deployment oracle cloud"],
+      github: "https://github.com/MujahidRamdhani/backend-test",
     },
     {
-      id: 4,
+      id: 5,
       image: survey,
       title: "Sistem survey kepuasan masyarakat terhadap pelayanan kantor kemenag kota bandung",
       points: ["php", "tailwind", "mysql", "chartjs"],
+      github: "https://github.com/MujahidRamdhani",
     },
     {
-      id: 4,
+      id: 6,
       image: furnicraft,
       title: "Ecommerce FURNICRAFT",
       points: ["Laravel", "Bootstrap", "Integration payment gateway midtrans", "integration raja ongkir", "mysql",],
+      github: "https://github.com/MujahidRamdhani/laravel10-ecomerce",
     },
   ];
   return (
@@ -51,15 +63,12 @@ export function Project() {
               <li key={index} className="li-point">{point}</li>
             ))}
           </ul>
-          <a href="https://github.com/MujahidRamdhani"><img src={githubIcon} alt="Github" className="github-icon" style={{ width: '80px', height: '100px' }}/></a>
+          <a href={item.github}><img src={githubIcon} alt="Github" className="github-icon" style={{ width: '60px', height: '100px' }}/></a>
           </div>
         </div>
       ))}
     </div>
-        
-
-
-      </div>
+   </div>
     </Container>
   );
 }
